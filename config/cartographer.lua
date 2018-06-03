15eb5db6be493481be07dca9b59ff4a81d449630
Gregory Meyer <gregjm@umich.edu>
2018-06-03 13:53:58 -0400

    modify cartographer config

--- config/cartographer.lua
+++ config/cartographer.lua
@@ -22,7 +22,7 @@ options = {
   tracking_frame = "imu",
   published_frame = "odom",
   odom_frame = "odom",
-  provide_odom_frame = true,
+  provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
   use_odometry = true,
   use_nav_sat = true,
@@ -36,10 +36,10 @@ options = {
   pose_publish_period_sec = 0.05,
   trajectory_publish_period_sec = 0.05,
   rangefinder_sampling_ratio = 1.0,
-  odometry_sampling_ratio = 0.5,
+  odometry_sampling_ratio = 1.0,
   fixed_frame_pose_sampling_ratio = 1.0,
   imu_sampling_ratio = 0.2,
-  landmarks_sampling_ratio = 0.1,
+  landmarks_sampling_ratio = 1.0,
 }
 
 MAP_BUILDER.use_trajectory_builder_3d = true
